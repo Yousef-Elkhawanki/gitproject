afcdafd6e01252dbc0824ee3a5ebba90a24cd250
Yousef-Elkhawanki <dev.elkhawanki@gmail.com>
2026-09-25 21:02:40 +0300

    test

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
-const userName = 'Yousef';
 function login(email: string, password: string) {
-  console.log('Logging in ...');
-  return true;
+  return { email, password };
 }
+login('test', 'test');
